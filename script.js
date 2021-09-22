169689c9003242f0635927d3a6cea3adda8c43f2
Portia Greene <pd12300@yahoo.com>
2021-09-21 20:33:43 -0400

    finished Javascript VariablesExercise

---
+++ script.js
@@ -0,0 +1,27 @@
+//1. Const
+
+//2.
+let num = 20;
+
+//3.
+num = 15;
+
+//4.
+
+num = num + 7;
+
+
+//5.
+num--
+
+//6.
+num = "This is Not a Number"
+console.log(num)
+
+// 7.
+let blank;
+// 8.
+console.log(blank);
+// 9.
+blank = null;
+
